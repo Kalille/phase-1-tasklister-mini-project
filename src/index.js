1105a08de0a52f63b036e2e901a20faac9d79b16
kalille Overton <>
2021-05-15 17:55:04 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
 });
+
+const taskList=document.getElementById('tasks')
+const taskForm =document.getElementById('create-task-form')
+taskForm.addEventListener('submit', (e)=>{
+
+  e.preventDefault();
+ const input = e.target.querySelector('#new-task-description').value
+  const pTag = document.createElement('p')
+  pTag.innerHTML = input
+  taskList.appendChild(pTag)
+  taskForm.reset()
+console.log(input)
+
+})
+// let button = document.createElement=('button');
+// button.setAttribute('value','CLEAR')
+// document.appendChild(button)
